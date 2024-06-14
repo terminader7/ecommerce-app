504f6f44d5f04998d4dfe17b79ec678f1da3bc41
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-14 15:30:31 -0700

    rednering products including the newest product added as well as a product grid

---
+++ app/error.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const ErrorPage = () => {
+  return <div>Something went wrong. Please refersh the page.</div>;
+};
+
+export default ErrorPage;
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "images.unsplash.com" }],
+    remotePatterns: [
+      { hostname: "images.unsplash.com" },
+      {
+        hostname: "plus.unsplash.com",
+      },
+    ],
   },
   experimental: {
     serverActions: true,
