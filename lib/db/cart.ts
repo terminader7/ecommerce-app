2c300311561d7a1d25b059d4bd77567f51269359
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-17 11:11:21 -0700

    updated cart.ts methods so that if you're anonymous your cart is created without a userId and if you're signed in we then bring back the cart that has a user id that matches your session id

--- lib/db/cart.ts
+++ lib/db/cart.ts
@@ -1,6 +1,8 @@
 import { cookies } from "next/dist/client/components/headers";
 import { prisma } from "./prisma";
 import { Cart, Prisma } from "@prisma/client";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 
 // Defining a type that represents a cart with its items and their associated products.
 export type CartWithProducts = Prisma.CartGetPayload<{
@@ -22,20 +24,31 @@ export type ShoppingCart = CartWithProducts & {
 };
 
 export const getCart = async (): Promise<ShoppingCart | null> => {
-  // Get the localCartId from cookies
-  const localCartId = cookies().get("localCartId")?.value;
+  const session = await getServerSession(authOptions);
 
-  // If there's a localCartId, try to find the cart in the database
-  const cart = localCartId
-    ? await prisma.cart.findUnique({
-        where: { id: localCartId },
-        include: {
-          items: {
-            include: { product: true },
+  let cart: CartWithProducts | null = null;
+
+  if (session) {
+    cart = await prisma.cart.findFirst({
+      where: { userId: session.user.id },
+      include: { items: { include: { product: true } } },
+    });
+  } else {
+    // Get the localCartId from cookies
+    const localCartId = cookies().get("localCartId")?.value;
+
+    // If there's a localCartId, try to find the cart in the database
+    cart = localCartId
+      ? await prisma.cart.findUnique({
+          where: { id: localCartId },
+          include: {
+            items: {
+              include: { product: true },
+            },
           },
-        },
-      })
-    : null; // If no localCartId, cart is null
+        })
+      : null; // If no localCartId, cart is null
+  }
 
   if (!cart) {
     return null;
@@ -53,13 +66,27 @@ export const getCart = async (): Promise<ShoppingCart | null> => {
 };
 
 export const createCart = async (): Promise<ShoppingCart> => {
+  const session = await getServerSession(authOptions);
+
+  let newCart: Cart;
+
+  if (session) {
+    newCart = await prisma.cart.create({
+      data: {
+        userId: session.user.id,
+      },
+    });
+  } else {
+    newCart = await prisma.cart.create({
+      data: {},
+    });
+
+    cookies().set("localCartId", newCart.id);
+  }
+
   // Create a new cart in the database
-  const newCart = await prisma.cart.create({
-    data: {},
-  });
 
   // Needs encryption, this won't be okay for production
-  cookies().set("localCartId", newCart.id);
 
   return {
     ...newCart,
