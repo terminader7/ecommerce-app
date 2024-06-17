d5052dff584d9c3cd01eccafb08ec544848fa4c6
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-17 10:01:14 -0700

    converted session provider to a client component and wrapped the layout in it, created env schema for nextauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer";
+import SessionProvider from "./SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main className="max-w-7-xl m-auto min-w-[300px] p-4">{children}</main>
-        <Footer />
+        <SessionProvider>
+          <Navbar />
+          <main className="max-w-7-xl m-auto min-w-[300px] p-4">
+            {children}
+          </main>
+          <Footer />
+        </SessionProvider>
       </body>
     </html>
   );
