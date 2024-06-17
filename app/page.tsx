301a3a11ad300339ca9dfcc0d655e59a837abb2a
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-17 12:53:36 -0700

    fixed check it out button

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default async function Home({
               <h1 className="text-5xl font-bold">{products[0].name}</h1>
               <p className="py-6">{products[0].description}</p>
               <Link
-                href={"/products" + products[0].id}
+                href={"/products/" + products[0].id}
                 className="btn btn-primary"
               >
                 Check it out!
