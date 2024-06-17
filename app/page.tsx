735617c7216d2b608a597aac67cbabc6f8c38fe7
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-17 12:22:47 -0700

    pagination now working properly and styled as well

--- app/page.tsx
+++ app/page.tsx
@@ -4,43 +4,63 @@ import { prisma } from "@/lib/db/prisma";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home() {
+interface HomeProps {
+  searchParams: { page: string };
+}
+
+export default async function Home({
+  searchParams: { page = "1" },
+}: HomeProps) {
+  const currentPage = parseInt(page);
+  const pageSize = 6;
+  const heroItemCount = 1;
+  const totalItemCount = await prisma.product.count();
+
+  const totalPages = Math.ceil((totalItemCount - heroItemCount) / pageSize);
+
   const products = await prisma.product.findMany({
     orderBy: {
       id: "desc",
     },
+    skip:
+      (currentPage - 1) * pageSize + (currentPage === 1 ? 0 : heroItemCount),
+    take: pageSize + (currentPage === 1 ? heroItemCount : 0),
   });
 
   return (
     <div className="flex flex-col items-center">
-      <div className="hero rounded-xl bg-base-200">
-        <div className="hero-content flex-col lg:flex-row">
-          <Image
-            src={products[0].imageUrl}
-            alt={products[0].name}
-            width={400}
-            height={800}
-            className="w-full max-w-sm rounded-lg shadow-2xl"
-            priority
-          />
-          <div>
-            <h1 className="text-5xl font-bold">{products[0].name}</h1>
-            <p className="py-6">{products[0].description}</p>
-            <Link
-              href={"/products" + products[0].id}
-              className="btn btn-primary"
-            >
-              Check it out!
-            </Link>
+      {currentPage === 1 && (
+        <div className="hero rounded-xl bg-base-200">
+          <div className="hero-content flex-col lg:flex-row">
+            <Image
+              src={products[0].imageUrl}
+              alt={products[0].name}
+              width={400}
+              height={800}
+              className="w-full max-w-sm rounded-lg shadow-2xl"
+              priority
+            />
+            <div>
+              <h1 className="text-5xl font-bold">{products[0].name}</h1>
+              <p className="py-6">{products[0].description}</p>
+              <Link
+                href={"/products" + products[0].id}
+                className="btn btn-primary"
+              >
+                Check it out!
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <div className="md: my-4 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-        {products.slice(1).map((product) => (
+        {(currentPage === 1 ? products.slice(1) : products).map((product) => (
           <ProductCard product={product} key={product.id} />
         ))}
       </div>
-      <Pagination currentPage={13} totalPages={99} />
+      {totalPages > 1 && (
+        <Pagination currentPage={currentPage} totalPages={totalPages} />
+      )}
     </div>
   );
 }
